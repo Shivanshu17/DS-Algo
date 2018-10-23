04fe02186ded9f3e0d19fee60bc640cc6693a729
Shivanshu17 <sharma.shivanshu98@gmail.com>
2018-10-23 18:27:04 +0530

    Addin the forgotten language problem

---
+++ Forgot_Lang.py
@@ -0,0 +1,30 @@
+t = int(input())
+
+while t>0:
+    t-=1
+    
+    N, K = input().split(" ")
+    N = int(N)
+    K = int(K)
+    Dict = [input().split(" ")]
+    
+    Check = [0]*N
+    
+    for i in range(K):
+        C = input().split(" ")
+        L = int(C[0])
+        del C[0]
+        
+        for k in C:
+            for z in Dict:
+                if k == z:
+                    Check[Dict.index(z)] = 1
+        
+        
+    for p in Check:
+        if p == 0:
+            p = "NO"
+        else:
+            p = "YES"
+    print(*Check)
+    
