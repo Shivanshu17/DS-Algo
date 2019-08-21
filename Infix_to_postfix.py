8a6d41512dd227b7bfdd76d06dc7dc1e4fece69f
Shivanshu17 <sharma.shivanshu98@gmail.com>
2019-08-21 19:14:31 +0530

    wrote the code for Infix to Postfix using Stack

--- Infix_to_postfix.py
+++ Infix_to_postfix.py
@@ -1,9 +1,40 @@
 from stack import *
 s = stack()
-s.push(12)
-s.push(13)
-s.push(44)
-t = s.pop()
-print(t)
-# s.push(12)
-## ss.size()
+
+a = list(input().split())
+
+def isOperand(self, ch):
+    return ch.isalpha()
+
+def notGreater(self, op):
+    precedence = {'+':1, '-':1, '*':2, '/':2, '^':3}
+    a = precedence[op]
+    b = s.peek()
+    return True if a>=b else False
+
+for i in a:
+    if self.isOperand(i):
+        print(i, end= '')
+    
+    elif i=='(':
+        s.push(i)
+        
+    elif i == ')':
+        while( not s.isEmpty() and s.peek!='('):
+            print(s.pop(), end ='')
+        if (s.isEmpty()):
+            print('Braces unmatched')
+        else:
+            s.pop()
+    
+    else:
+        while(not s.isEmpty() and self.notGreater(i)):
+            print(s.pop(), end = '')
+        
+        s.push(i)
+
+while(not s.isEmpty()):
+    print(s.pop(), end ='')
+
+
+    
--- stack.py
+++ stack.py
@@ -13,3 +13,16 @@ class stack:
     def size(self):
         return len(self.stack)
     
+    def peek(self):
+        if len(self.stack) == 0:
+            return None
+        t = self.stack.pop()
+        self.stack.push(t)
+        return(t)
+    
+    def isEmpty(self):
+        if len(self.stack) == 0:
+            return True
+        return False
+    
+    
